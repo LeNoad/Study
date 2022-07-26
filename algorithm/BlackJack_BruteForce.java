92a75f249d8f4a863de182e069e84a6c100fda84
DESKTOP-OSTSJCT\jkds <asdasd24242424@gmail.com>
2022-07-26 10:30:28 +0900

    Scanner 예외처리

--- algorithm/BlackJack_BruteForce.java
+++ algorithm/BlackJack_BruteForce.java
@@ -39,5 +39,6 @@ public class BlackJack_BruteForce {
             }
         }
         System.out.println(max);
+        sc.close();
     }
 }
--- algorithm/Palindrome.java
+++ algorithm/Palindrome.java
@@ -12,7 +12,6 @@ public class Palindrome {
         } else {
             System.out.println("Not Palinedrome");
         }
-
-    
+        sc.close();
     }
 }
